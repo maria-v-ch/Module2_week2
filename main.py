6f0faf95916eb5a1bb6107d222943322fd37ca70
maria-v-ch <159662753+maria-v-ch@users.noreply.github.com>
2024-08-23 17:00:15 +0300

    User can now input text for printing it out

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ class Document:
     def __init__(self, printer):
         self.printer = printer
 
-    def print_document(self):
-        return self.printer.print_page()
+    def print_document(self, text):
+        return self.printer.print_page(text)
 
 
 class InkjetPrinter:
     @staticmethod
-    def print_page():
-        return 'Inkjet printer printing ...'
+    def print_page(text):
+        return f'Inkjet printer printing: {text}'
 
 
 class LaserPrinter:
     @staticmethod
-    def print_page():
-        return 'Laser printer printing ...'
+    def print_page(text):
+        return f'Laser printer printing: {text}'
 
 
 if __name__ == '__main__':
+    
+    text_to_print = input('Enter the text to print it: ')
+    
     document = Document(LaserPrinter())
-    print(document.print_document())
+    print(document.print_document(text_to_print))
+    
     document = Document(InkjetPrinter())
-    print(document.print_document())
+    print(document.print_document(text_to_print))
